7fef871a9be3a09ace061f2a08698a2afccd4fc1
chatterboxn18 <lambocreates@gmail.com>
2017-07-13 11:45:44 -0700

    pulled the unity folder over

---
+++ EverKristen/Assets/Scripts/movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    //private bool hitRightWall = false;
+    //private bool hitLeftWall = false;
+    // Use this for initialization
+    public GameObject poop;
+    private int timer = 0;
+    public float movementSpeed = 5;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey(KeyCode.RightArrow) ^ Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (Input.GetKey(KeyCode.RightArrow)) //&& !hitRightWall)
+            {
+                this.transform.Translate(new Vector2(this.movementSpeed * Time.deltaTime, 0));
+                //hitLeftWall = false;
+
+            }
+            else
+            {
+                this.transform.Translate(new Vector2(-this.movementSpeed * Time.deltaTime, 0));
+            }
+            
+        }
+        if (timer == 4)
+        {
+            GameObject bullets = (GameObject)Instantiate(poop, new Vector3(0, 1f, 0) + this.transform.position, Quaternion.identity);
+            timer = 0;
+        }
+        timer++;
+        Debug.Log(timer);
+        
+        
+	}
+
+}
---
+++ EverKristen/Assets/Scripts/poopbullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poopbullet : MonoBehaviour {
+    public float bulletSpeed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(new Vector2(0, bulletSpeed * Time.deltaTime));
+	}
+
+   
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag != "bullet")
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
